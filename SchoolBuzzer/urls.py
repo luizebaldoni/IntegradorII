a53a5c7168e1e9f60bd3833f8c0d486d8f655f7d
Vinicios <114757859+XDuckBlackX@users.noreply.github.com>
2025-06-03 00:13:42 -0300

    Update urls.py
    Criação de novas rotas e atualização das existentes

--- SchoolBuzzer/urls.py
+++ SchoolBuzzer/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from app.views import HomeView  # Certifique-se de importar corretamente a HomeView
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Rota para o painel administrativo
-    path('', HomeView.as_view(), name='home'),  # Página inicial
+    path('admin/', admin.site.urls),
+    path('', include('app.urls')),   # todas as rotas do "app"
 ]
